c0028545246d753b32f1dbe249baaaf22e455eba
Marco Flores <marco.flores85@hotmail.com>
2018-12-13 21:51:38 -0600

    modificaciones finales

--- js/ahorcado.js
+++ js/ahorcado.js
@@ -4,7 +4,7 @@ var palabra = "";
 var rand;
 var oculta = [];
 var hueco = document.getElementById("palabra");
-var cont = 6;
+var cont = 0;
 var buttons = document.getElementsByClassName('letra');
 var btnInicio = document.getElementById("reset");
 
@@ -21,5 +21,70 @@ function generarLineas(){
   hueco.innerHTML = oculta.join(" ");
 }
 
-generarPalabra();
-generarLineas();
+
+function generaABC (a,z) {
+  document.getElementById("abcdario").innerHTML = "";
+  var i = a.charCodeAt(0), j = z.charCodeAt(0);
+  var letra = "";
+  for( ; i<=j; i++) {
+    letra = String.fromCharCode(i).toUpperCase();
+    document.getElementById("abcdario").innerHTML += "<button value='" + letra + "' onclick='intento(\"" + letra + "\")' class='letra' id='"+letra+"'>" + letra + "</button>";
+    if(i==110) {
+      document.getElementById("abcdario").innerHTML += "<button value='Ñ' onclick='intento(\"Ñ\")' class='letra' id='"+letra+"'>Ñ</button>";
+    }
+  }
+}
+
+function intento(letra){
+  document.getElementById(letra).disable = true;
+  if (palabra.indexOf(letra) != -1){
+    for (i = 0; i < palabra.length; i++){
+      if(palabra[i]==letra) oculta[i] = letra;
+    }
+    hueco.innerHTML = oculta.join(" ")
+    document.getElementById ("acierto").innerHTML = "Bien!";
+    document.getElementById ("acierto").className += "acierto verde";
+  }else{
+    cont++;
+    document.getElementById ("intentos").innerHTML = cont;
+    document.getElementById("intentos").innerHTML = cont;
+    document.getElementById("acierto").innerHTML = "Fallo!";
+    document.getElementById("acierto").className += "acierto rojo";
+    document.getElementById("image"+ cont).style.visibility = "visible"+ cont;
+ }
+ compruebaFin();
+  setTimeout(function () { 
+    document.getElementById("acierto").className = " ";
+  }, 800);
+
+}
+
+function compruebaFin(){
+  if (oculta.indexOf(" ") == -1) {
+    document.getElementById("msg-final").innerHTML = "felicidades !!";
+    document.getElementById("msg-final").className += "zoom-in";
+    document.getElementById("palabra").className += " encuadre";
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+    document.getElementById("reset").innerHTML = "Empezar";
+    btnInicio.onclick = function() { location.reload() };
+  }else if( cont == 0 ) {
+    document.getElementById("msg-final").innerHTML = "Game Over";
+    document.getElementById("msg-final").className += "zoom-in";
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+    document.getElementById("reset").innerHTML = "Empezar";
+    btnInicio.onclick = function () { location.reload() };
+}
+}
+function inicio() {
+  generarPalabra();
+  generarLineas(palabra.length);
+  cont = 6;
+  document.getElementById("intentos").innerHTML=cont;
+}
+
+// Iniciar
+window.onload = inicio();
